eebc0a7e72d0de3a67bca8706e25739490ca3c3d
niki <nikoletadal@gmail.com>
2017-11-18 16:05:02 +0200

    get names; get/update information

--- backend/BerryBear/urls.py
+++ backend/BerryBear/urls.py
@@ -4,5 +4,9 @@ from . import views
 urlpatterns = [
     url(r'^$', views.index, name='index'),
     url(r'^move/', views.move, name='move'),
+    url(r'^programs_name/', views.get_programs_name, name='names'),
+    url(r'^program_info/', views.get_program_info, name='information'),
+    url(r'^update_program_info/', views.update_program_info, name='update'),
+    url(r'^run/', views.run, name='run'),
 
 ]
--- backend/BerryBear/views.py
+++ backend/BerryBear/views.py
@@ -6,6 +6,8 @@ from django.http import HttpResponse
 from django.http import JsonResponse
 import json
 from django.views.decorators.csrf import csrf_exempt
+from .models import Program
+from django.core import serializers
 
 # Create your views here.
 
@@ -16,7 +18,55 @@ def index(request):
 
 @csrf_exempt
 def move(request):
-	data = json.loads(request.body)
-	#TODO make function for moving the bear
-	return HttpResponse(data["direction"])
-	#return JsonResponse(data)
+    data = json.loads(request.body)
+    print data["direction"]
+    #TODO make function for moving the bear
+    return HttpResponse(data["direction"])
+    #return JsonResponse(data)
+
+
+
+def get_programs_name(request):
+    programs = Program.objects.all()
+
+    response = []
+    for p in programs:
+        response.append({
+                    "id": p.id,
+                    "name" : p.name
+                })
+    return JsonResponse(response, safe = False)
+
+
+@csrf_exempt
+def get_program_info(request):
+    id = json.loads(request.body)
+    info = Program.objects.get(id = id["id"])
+
+    data = {
+        "id" : info.id,
+        "name" : info.name,
+        "commands" : info.commands
+    }
+
+    return JsonResponse(data)
+
+
+
+
+@csrf_exempt
+def update_program_info(request):
+    new_info = json.loads(request.body)
+
+    Program.objects.filter(id = new_info["id"]).update(name = new_info["name"])
+    Program.objects.filter(id = new_info["id"]).update(commands = new_info["commands"])    
+
+    return HttpResponse("update")
+
+@csrf_exempt
+def run(request):
+    id = (json.loads(request.body))["id"]
+    commands = Program.objects.get(id= id).commands.split(" ")
+    for c in commands:
+        print c
+    return HttpResponse(id)
